e9cf62bcd4b6f58487903692418fc5223ddad766
kirilldaniakin <81981425+kirilldaniakin@users.noreply.github.com>
2021-04-11 20:17:52 +0300

    Update getDataHW1.py

--- scripts/getDataHW1.py
+++ scripts/getDataHW1.py
@@ -3,15 +3,19 @@ import pandas as pd
 def get_data(data_path="../data",testData = False):
     assert os.path.isfile(data_path), f"{os.path.realpath(data_path)} : File not exist"
     
+    # read data
     train_data = pd.read_csv(data_path+"/train_eng.csv", engine='python' ,encoding = "latin-1")
     test_data = pd.read_csv(data_path+"/test_eng.csv", engine='python' ,encoding = "latin-1")
 
+    # transform labels 'F', 'M' into 0,1 correspondingly
     labels_keys = {value: i for i, (value, count) in enumerate(train_data.Gender.value_counts().items())}
     train_data['Gender'] = train_data['Gender'].apply(lambda x: labels_keys.get(x))
     test_data['Gender'] = test_data['Gender'].apply(lambda x: labels_keys.get(x))
     
+    #get max name length
     max_len = len(max(train_data.iloc[:,0], key=len))
 
+    # add whitespaces in between charachters (see report)
     for name in range(len(list(train_data.iloc[:,0]))):
       names=''
       for char in range(len(train_data.Name[name])):
@@ -26,6 +30,7 @@ def get_data(data_path="../data",testData = False):
       test_data.Name[name] = names
     print(test_data.iloc[:,0])
     
+    # to tensors
     train_dataset = tf.data.Dataset.from_tensor_slices(
         (
             tf.cast(train_data['Name'].values, tf.string),
@@ -38,6 +43,7 @@ def get_data(data_path="../data",testData = False):
                 tf.cast(test_data['Gender'].values, tf.int64)
             ))
     
+    # shuffle and batch
     BUFFER_SIZE = 10000
     BATCH_SIZE = 64
     train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
