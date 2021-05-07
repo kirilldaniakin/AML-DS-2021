3e300ce18a272d81f8d3e63b7a51e08aa664b944
kirilldaniakin <81981425+kirilldaniakin@users.noreply.github.com>
2021-05-08 00:19:36 +0300

    Update getData.py

--- scripts/getData.py
+++ scripts/getData.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import os
+import numpy as np
 
+def get_data(data_path="../data"):
+    assert os.path.isfile(data_path+"/train.csv"), f"{os.path.realpath(data_path)} : File does not exist"
+    assert os.path.isfile(data_path+"/test.csv"), f"{os.path.realpath(data_path)} : File does not exist"
+    # read data
+    df_ratings = pd.read_csv(
+    'train.csv',
+    usecols=['userId', 'movieId', 'rating'],
+    dtype={'userId': 'int32', 'movieId': 'int32', 'rating': 'float32'})
+
+    df_ratings_test = pd.read_csv(
+    'test.csv',
+    usecols=['userId', 'movieId', 'rating'],
+    dtype={'userId': 'int32', 'movieId': 'int32', 'rating': 'float32'})
+    
+  return df_ratings, df_ratings_test
+
+if __name__ == '__main__':
+  pass
