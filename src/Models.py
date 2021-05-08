b76081f45796649fc23acafe557fb84852c91cc7
kirilldaniakin <81981425+kirilldaniakin@users.noreply.github.com>
2021-05-08 18:04:30 +0300

    Update Models.py

--- src/Models.py
+++ src/Models.py
@@ -74,17 +74,19 @@ class MF(nn.Module):
         #print(target)
         if R is None:
             R = self.R
+        loss_mse = 0    
         for i in range(x.size()[0]):
             #print(torch.index_select(torch.index_select(x,0,torch.tensor([i])), 1, torch.tensor([0])))
             #print(torch.index_select(torch.index_select(x,0,torch.tensor([i])), 1, torch.tensor([1])))
             print("i=", i)
             try:
                 if torch.index_select(torch.index_select(R, 0, torch.tensor(torch.index_select(torch.index_select(x,0,torch.tensor([i])), 1, torch.tensor([0])).item())),1,torch.tensor(torch.index_select(torch.index_select(x,0,torch.tensor([i])), 1, torch.tensor([1])).item())).item()!=0:
-                    loss_mse = F.mse_loss(prediction, target.squeeze())
+                    loss_mse += F.mse_loss(prediction, target.squeeze())
                 else:
-                    loss_mse = torch.tensor(0)
+                    loss_mse += torch.tensor(0)
             except:
-                loss_mse = torch.tensor(0)
+                loss_mse += torch.tensor(0)
+        loss_mse /= x.size()[0]    
         # Compute L2 regularization over user (P) and item (Q) matrices
         prior_user = l2_regularize(self.user.weight) * self.c_vector
         prior_item = l2_regularize(self.item.weight) * self.c_vector
